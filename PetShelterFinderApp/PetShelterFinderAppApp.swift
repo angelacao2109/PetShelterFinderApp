d55450ae8ca23783786c858131bef8dcda4a4534
angelacao2109 <angelacao2109@yahoo.com>
2023-11-13 11:51:27 -0500

    Initial Commit

---
+++ PetShelterFinderApp/PetShelterFinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetShelterFinderAppApp.swift
+//  PetShelterFinderApp
+//
+//  Created by Angela  on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PetShelterFinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
